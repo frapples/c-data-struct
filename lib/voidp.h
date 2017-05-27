7685913e8d6138b990d5a67ca71ce740fbfbaf57
Frapples <isfrapples@outlook.com>
2017-05-27 10:21:01 +0800

    完善voidp测试

--- lib/voidp.h
+++ lib/voidp.h
@@ -5,36 +5,42 @@
 /* 支持使用void * 储存Long和bool类型的数据。 */
 static long   voidp2long   (void*  voidp);
 static bool   voidp2bool   (void*  voidp);
-static void*  voidp2pointer(void*  voidp);
 static void*  long2voidp   (long);
 static void*  bool2voidp   (bool);
 
+union __cast {
+    void* voidp;
+    long vlong;
+    bool vbool;
+};
 
 static inline long voidp2long(void*  voidp)
 {
-    return (long)voidp;
+    union __cast cast;
+    cast.voidp = voidp;
+    return cast.vlong;
 }
 
 static inline bool voidp2bool(void*  voidp)
 {
-    return (bool)voidp;
+    union __cast cast;
+    cast.voidp = voidp;
+    return cast.vbool;
 }
-
-static inline void* voidp2pointer(void*  voidp)
-{
-    return voidp;
-}
-
 
 
 static inline void*  long2voidp(long long_data)
 {
-    return (void* )long_data;
+    union __cast cast;
+    cast.vlong = long_data;
+    return cast.voidp;
 }
 
 static inline void*  bool2voidp(bool bool_data)
 {
-    return (void* )bool_data;
+    union __cast cast;
+    cast.vbool = bool_data;
+    return cast.voidp;
 }
 
 #endif
--- test/test_main.c
+++ test/test_main.c
@@ -1,19 +1,30 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
+#include <stdlib.h>
+#include <time.h>
 #include "google/cmockery.h"
 #include "voidp.h"
 
 
 void test_voidp(void **state)
 {
-    assert_true(true);
+    int count = 100;
+    while (count-- > 0) {
+        long n = rand();
+        assert_true(n == (voidp2long(long2voidp(n))));
+
+        bool b = n % 2;
+        assert_true(b == (voidp2bool(bool2voidp(b))));
+    }
 }
 
 int main(void)
 {
+    srand(time(NULL));
+
     const UnitTest tests[] = {
-            unit_test(test_voidp),
+        unit_test(test_voidp),
     };
     return run_tests(tests);
 }
