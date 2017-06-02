0e056f18a7159c6fbabe0fcabdd7efe6e42e9d12
Frapples <isfrapples@outlook.com>
2017-06-02 09:05:17 +0800

    hashtable初步实现

---
+++ lib/hashtable.c
@@ -0,0 +1,99 @@
+#include "hashtable.h"
+
+#include <string.h>
+#include <assert.h>
+#include "alloc.h"
+
+#define INITIALIZE_BUCKET_SIZE 7
+hashtable_t* hashtable_create(HashFunc hash_function, CmpFunc cmp_function)
+{
+    hashtable_t* table = FDS_NEW(hashtable_t, 1);
+    table->size = INITIALIZE_BUCKET_SIZE;
+    table->len = 0;
+    table->hash_function = hash_function;
+    table->cmp_function = cmp_function;
+    table->buckets = FDS_NEW(hashtable_bucket_t*, INITIALIZE_BUCKET_SIZE);
+    for (size_t i = 0; i < table->len; i++) {
+        table->buckets[i] = NULL;
+    }
+    return table;
+}
+
+hashtable_t* hashtable_create_strkey()
+{
+    return hashtable_create(hashtable_str_hash, (CmpFunc)strcmp);
+}
+
+void hashtable_put(hashtable_t* table, void* key, void* value)
+{
+    size_t hash = table->hash_function(key, table->size);
+    hashtable_bucket_t** head = table->buckets + hash;
+
+    for (hashtable_bucket_t* it = *head; it != NULL; it = it->next) {
+        if (table->cmp_function(key, it->key)) {
+            it->value = value;
+            return;
+        }
+    }
+
+    hashtable_bucket_t* node = FDS_NEW(hashtable_bucket_t, 1);
+    node->key = key;
+    node->value = value;
+
+    node->next = *head;
+    *head = node;
+
+    table->len++;
+}
+
+bool hashtable_exists(hashtable_t* table, void* key)
+{
+    size_t hash = table->hash_function(key, table->size);
+
+    for (hashtable_bucket_t* it = table->buckets[hash]; it != NULL; it = it->next) {
+        if (table->cmp_function(key, it->key)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void* hashtable_get(hashtable_t* table, void* key, void* default_value)
+{
+    size_t hash = table->hash_function(key, table->size);
+
+    for (hashtable_bucket_t* it = table->buckets[hash]; it != NULL; it = it->next) {
+        if (table->cmp_function(key, it->key)) {
+            return it->value;
+        }
+    }
+    return default_value;
+}
+
+size_t hashtable_len(hashtable_t* table)
+{
+    return table->len;
+}
+
+void hashtable_each(hashtable_t* table, HashtableEachCallback callback, void* callback_data)
+{
+    for (size_t i = 0; i < table->len; i++) {
+        hashtable_bucket_t* head = table->buckets[i];
+        for (hashtable_bucket_t* it = head; it != NULL; it = it->next) {
+            (*callback)(it->key, it->value, callback_data);
+        }
+    }
+}
+
+size_t hashtable_str_hash(const void* str, size_t table_size)
+{
+    const char* s = (const char*)str;
+
+    size_t hash = 0;
+    while (*s != '\0') {
+        /* 旧值乘以32加上字符的ASCII  */
+        hash = (hash << 5) + *s;
+        s++;
+    }
+    return hash % table_size;
+}
--- lib/hashtable.h
+++ lib/hashtable.h
@@ -8,19 +8,22 @@ typedef struct hashtable_bucket_s{
     struct hashtable_bucket_s* next;
     void* key;
     void* value;
-} hastable_bucket_t;
+} hashtable_bucket_t;
 
-typedef size_t (*HashFunc)(void *, size_t);
+typedef size_t (*HashFunc)(const void *, size_t);
+typedef int (*CmpFunc)(const void *, const void*);
 
 typedef struct {
     size_t size;
+    size_t len;
     HashFunc hash_function;
-    hastable_bucket_t* buckets;
+    CmpFunc cmp_function;
+    hashtable_bucket_t** buckets;
 } hashtable_t;
 
 typedef void (*HashtableEachCallback)(void* key, void* value, void* data);
 
-hashtable_t* hashtable_create(HashFunc hash_function);
+hashtable_t* hashtable_create(HashFunc hash_function, CmpFunc cmp_function);
 hashtable_t* hashtable_create_strkey();
 void hashtable_put(hashtable_t* table, void* key, void* value);
 bool hashtable_exists(hashtable_t* table, void* key);
@@ -28,7 +31,9 @@ void* hashtable_get(hashtable_t* table, void* key, void* default_value);
 
 size_t hashtable_len(hashtable_t* table);
 
-void hashtable_each(hashtable_t* table, HashtableEachCallback callback);
+void hashtable_each(hashtable_t* table, HashtableEachCallback callback, void* callback_data);
+
+size_t hashtable_str_hash(const void* str, size_t table_size);
 
 
 #endif /* __HASHTABLE_H__RAND6950493206809747 */
