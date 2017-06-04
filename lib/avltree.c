eb7966cd7a8bda42e17ffa7024feacb6d3270b06
Frapples <isfrapples@outlook.com>
2017-06-04 14:51:30 +0800

    avltree设计和编码部分简单函数

--- lib/avltree.c
+++ lib/avltree.c
@@ -5,7 +5,7 @@
 #include "alloc.h"
 
 static void insert(avltree_node_t** root, avltree_node_t* key, avltree_node_t* value, CmpFunc cmp_function);
-static avltree_node_t** find(avltree_node_t** root, avltree_node_t* key, CmpFunc cmp_function);
+static avltree_node_t* find(avltree_node_t* root, avltree_node_t* key, CmpFunc cmp_function);
 static void destory(avltree_node_t* root);
 
 avltree_t* avltree_create(CmpFunc cmp_function)
@@ -28,13 +28,13 @@ void avltree_put(avltree_t* tree, void* key, void* value)
 
 bool avltree_exists(avltree_t* tree, void* key)
 {
-    return *(find(&(tree->root), key, tree->cmp_function)) != NULL;
+    return find(tree->root, key, tree->cmp_function) != NULL;
 }
 
 void* avltree_get(avltree_t* tree, void* key, void* default_value)
 {
-    avltree_node_t** p_node = find(&(tree->root), key, tree->cmp_function);
-    return *p_node == NULL ? default_value : (*p_node)->value;
+    avltree_node_t* node = find(tree->root, key, tree->cmp_function);
+    return node == NULL ? default_value : node->value;
 }
 
 void avltree_remove(avltree_t* tree, void* key)
@@ -47,4 +47,35 @@ void avltree_destory(avltree_t* tree)
 {
     destory(tree->root);
     fds_free(tree);
+}
+
+static void insert(avltree_node_t** root, avltree_node_t* key, avltree_node_t* value, CmpFunc cmp_function)
+{
+    /* TODO */
+    assert(false);
+}
+
+static avltree_node_t* find(avltree_node_t* root, avltree_node_t* key, CmpFunc cmp_function)
+{
+    if (root == NULL) {
+        return root;
+    }
+
+    int cmp = cmp_function(key, root->key);
+    if (cmp < 0) {
+        return find(root->left, key, cmp_function);
+    } else if (cmp > 0) {
+        return find(root->right, key, cmp_function);
+    } else {
+        return root;
+    }
+}
+
+static void destory(avltree_node_t* root)
+{
+    if (root != NULL) {
+        destory(root->left);
+        destory(root->right);
+        fds_free(root);
+    }
 }
