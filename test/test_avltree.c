02dd8b8dab60bbecc6687f51008fe078b33b0fa0
Frapples <isfrapples@outlook.com>
2017-06-05 00:07:39 +0800

    添加avltree的平衡性测试

--- test/test_avltree.c
+++ test/test_avltree.c
@@ -11,7 +11,10 @@
 #include "avltree.h"
 #include "./utils.h"
 
+static inline int max(int a, int b);
+
 int int_cmp(const void* a, const void* b);
+int check_balance(avltree_node_t* root);
 
 void test_avltree(void **state)
 {
@@ -31,9 +34,34 @@ void test_avltree(void **state)
     for (int i = size; i < 2 * size; i++) {
         assert_false(avltree_exists(tree, long2voidp(i)));
     }
+
+    check_balance(tree->root);
+
+    avltree_destory(tree);
+}
+
+int check_balance(avltree_node_t* root)
+{
+    if (root == NULL) {
+        return -1;
+    }
+
+    int left_height = check_balance(root->left);
+    int right_height = check_balance(root->right);
+
+    assert_int_equal(max(left_height, right_height) + 1, root->height);
+
+    assert_true(abs(left_height - right_height) <= 1);
+
+    return root->height;
 }
 
 int int_cmp(const void* a, const void* b)
 {
     return voidp2long(a) - voidp2long(b);
+}
+
+static inline int max(int a, int b)
+{
+    return a > b ? a : b;
 }
