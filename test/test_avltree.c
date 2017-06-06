ea2de49dda0399ded32a7b0defdcdc7ac0d91916
Frapples <isfrapples@outlook.com>
2017-06-06 11:25:51 +0800

    添加avltree的remove测试

--- test/test_avltree.c
+++ test/test_avltree.c
@@ -16,6 +16,7 @@ static inline int max(int a, int b);
 int int_cmp(const void* a, const void* b);
 static int check_balance(avltree_node_t* root);
 static void test(int* arr, int size);
+static void test_remove(int* arr, int size);
 
 void test_avltree(void **state)
 {
@@ -23,6 +24,10 @@ void test_avltree(void **state)
     test(range(0, 2 * size, 1), size);
     test(range(2 * size, 0, -1), size);
     test(rand_array(2 * size), size);
+
+    test_remove(range(0, 2 * size, 1), size);
+    test_remove(range(2 * size, 0, -1), size);
+    test_remove(rand_array(2 * size), size);
 }
 
 static void test(int* arr, int size)
@@ -40,6 +45,32 @@ static void test(int* arr, int size)
 
     for (int i = size; i < 2 * size; i++) {
         assert_false(avltree_exists(tree, long2voidp(arr[i])));
+    }
+
+    check_balance(tree->root);
+
+    avltree_destory(tree);
+}
+
+static void test_remove(int* arr, int size)
+{
+    avltree_t* tree = avltree_create(int_cmp);
+
+    for (int i = 0; i < 2 * size; i++) {
+        avltree_put(tree, long2voidp(arr[i]), long2voidp(arr[i]));
+    }
+
+    for (int i = 0; i < size; i++) {
+        avltree_remove(tree, long2voidp(arr[i]));
+    }
+
+    for (int i = 0; i < size; i++) {
+        assert_false(avltree_exists(tree, long2voidp(arr[i])));
+    }
+
+    for (int i = size; i < 2 * size; i++) {
+        assert_true(avltree_exists(tree, long2voidp(arr[i])));
+        assert_int_equal(voidp2long(avltree_get(tree, long2voidp(arr[i]), 0)), arr[i]);
     }
 
     check_balance(tree->root);
