16d6a716c403b10493df9ae618dfa1fdec7bd6a7
Frapples <isfrapples@outlook.com>
2017-07-01 20:42:57 +0800

    使rbtree的旋转代码更易复用，抽取插入部分的颜色判断和修改逻辑

--- lib/rbtree.c
+++ lib/rbtree.c
@@ -9,7 +9,7 @@
 static rbtree_node_t* find(rbtree_node_t* node, void* key, CmpFunc cmp_function);
 static void insert(rbtree_node_t** p_root, void* key, void* value, CmpFunc cmp_function);
 static rbtree_node_t* create_node(void* key, void* value, char color);
-static void rotate(rbtree_node_t** p_node);
+static void rotate_inserting(rbtree_node_t** p_node);
 inline static char node_color(rbtree_node_t* node);
 inline static rbtree_node_t** next(rbtree_node_t* node, void* key, CmpFunc cmp_function);
 
@@ -100,7 +100,7 @@ static void insert(rbtree_node_t** p_root, void* key, void* value, CmpFunc cmp_f
                     /*  旋转会改变树结构，因此，p_grandparent, p_parent, p_node指向的节点父子关系失效 */
                     /* 一种方法是，在旋转之后，把p_node 设为p_grandparent, 从*p_grandparent到p_node的循环再来一次 */
                     /* 显然， 重新从*p_grandparent循环到*p_node的过程中，不会发生新的旋转，循环也不会结束 */
-                    rotate(p_grandparent);
+                    rotate_inserting(p_grandparent);
                     p_node = p_grandparent;
                     p_grandparent = NULL;
                     p_parent = NULL;
@@ -120,7 +120,7 @@ static void insert(rbtree_node_t** p_root, void* key, void* value, CmpFunc cmp_f
 
     *p_node = create_node(key, value, COLOR_RED);
     if (p_grandparent != NULL) {
-        rotate(p_grandparent);
+        rotate_inserting(p_grandparent);
     } else {
         assert(p_parent == NULL || p_parent == p_root);
     }
@@ -179,16 +179,20 @@ static void single_rotate_with_right(rbtree_node_t** p_root);
 static void double_roate_with_left(rbtree_node_t** p_root);
 static void double_roate_with_right(rbtree_node_t** p_root);
 
-static void rotate(rbtree_node_t** p_node)
+/* 插入过程中的发生的旋转 */
+static void rotate_inserting(rbtree_node_t** p_node)
 {
     assert(*p_node != NULL);
 
+    bool rotated = true;
     if (node_color((*p_node)->left) == COLOR_RED) {
         rbtree_node_t* son = (*p_node)->left;
         if (node_color(son->left) == COLOR_RED) { // LL型单旋转
             single_rotate_with_left(p_node);
         } else if (node_color(son->right) == COLOR_RED) { // LR型双旋转
             double_roate_with_left(p_node);
+        } else {
+            rotated = false;
         }
     } else if (node_color((*p_node)->right) == COLOR_RED) {
         rbtree_node_t* son = (*p_node)->right;
@@ -196,7 +200,17 @@ static void rotate(rbtree_node_t** p_node)
             double_roate_with_right(p_node);
         } else if (node_color(son->right) == COLOR_RED) { // RR型单旋转
             single_rotate_with_right(p_node);
+        } else {
+            rotated = false;
         }
+    } else {
+        rotated = false;
+    }
+
+    if (rotated) {
+        (*p_node)->color = COLOR_BLACK;
+        (*p_node)->left->color = COLOR_RED;
+        (*p_node)->right->color = COLOR_RED;
     }
 }
 
@@ -207,9 +221,6 @@ static void single_rotate_with_left(rbtree_node_t** p_root) {
     old_root->left = new_root->right;
     new_root->right = old_root;
     *p_root = new_root;
-
-    new_root->color = COLOR_BLACK;
-    old_root->color = COLOR_RED;
 }
 
 static void single_rotate_with_right(rbtree_node_t** p_root) {
@@ -218,9 +229,6 @@ static void single_rotate_with_right(rbtree_node_t** p_root) {
     old_root->right = new_root->left;
     new_root->left = old_root;
     *p_root = new_root;
-
-    new_root->color = COLOR_BLACK;
-    old_root->color = COLOR_RED;
 }
 
 static void double_roate_with_left(rbtree_node_t** p_root) {
